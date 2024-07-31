6c6670fbd76b39917f53084ac49ea5baa1d46993
동영 <arhaxoda@naver.com>
2024-07-31 16:53:06 +0900

    240731

--- flutter_example07/lib/main.dart
+++ flutter_example07/lib/main.dart
@@ -1,46 +1,112 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const AppBarApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class AppBarApp extends StatelessWidget {
-  const AppBarApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const AppBarExample(),
+      title: 'Flutter App',
+      home: const MainPage(),
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue).copyWith(
-          surfaceTint: Colors.transparent,
-        ),
-        appBarTheme: AppBarTheme(
-          elevation: 4.0,
-          backgroundColor: Colors.blue,
-          titleTextStyle: const TextStyle(fontSize: 26, color: Colors.white),
-          shadowColor: Colors.deepPurple.withOpacity(0.5),
-        ),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
     );
   }
 }
 
-class AppBarExample extends StatelessWidget {
-  const AppBarExample({super.key});
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List<String> friendList = [
+    "Andrew",
+    "Brian",
+    "Catherine",
+    "Wilson",
+    "Raul",
+    "Daniel",
+    "John",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final isUsingMaterial3 = Theme.of(context).useMaterial3;
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(isUsingMaterial3 ? 'Using Material 3' : 'Using Material 2'),
+        title: const Text("Material 3 Design"),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        centerTitle: true,
+        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 24),
       ),
-      body: const Center(
-        child: Text(
-          "I love Flutter.",
-          style: TextStyle(fontSize: 24),
+      body: ListView.builder(
+          itemCount: friendList.length,
+          itemBuilder: (context, index) {
+            return cardContainer(name: friendList[index], number: index + 1);
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showFloatingDialog();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget cardContainer({String name = "", int number = 0}) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: ListTile(
+        title: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.all(12),
+          child: Text("$number. $name", style: const TextStyle(fontSize: 20)),
+        ),
+        subtitle: Row(
+          children: [
+            TextButton(
+              onPressed: () {},
+              child: const Text("Details"),
+            ),
+            FilledButton.tonal(
+              onPressed: () {},
+              child: const Text("Follow"),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  void showFloatingDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Center(child: Text("Do you want to add a friend?")),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("No"),
+            ),
+            FilledButton(onPressed: () {}, child: const Text("Yes")),
+          ],
+        );
+      },
     );
   }
 }
---
+++ flutter_example08/lib/main.dart
@@ -0,0 +1,218 @@
+import 'dart:ffi';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const myApp());
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("다양한 flutter의 입력 알아보기"),
+          backgroundColor: Colors.blue,
+          elevation: 4.0,
+          shadowColor: Colors.deepPurple,
+        ),
+        body: const Body(),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        TestCheckBox(),
+        TestRadioButton(),
+        TestSlider(),
+        TestSwitch(),
+        TestPopupMenu(),
+      ],
+    );
+  }
+}
+
+class TestCheckBox extends StatefulWidget {
+  const TestCheckBox({super.key});
+
+  @override
+  State<TestCheckBox> createState() => _TestCheckBoxState();
+}
+
+class _TestCheckBoxState extends State<TestCheckBox> {
+  late List<bool> values;
+
+  @override
+  void initState() {
+    super.initState();
+    values = [false, false, false];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Checkbox(value: values[0], onChanged: (value) => changeValue(0, value: value)),
+        Checkbox(value: values[1], onChanged: (value) => changeValue(1, value: value)),
+        Checkbox(value: values[2], onChanged: (value) => changeValue(2, value: value)),
+      ],
+    );
+  }
+
+  void changeValue(int index, {bool? value = false}) {
+    setState(() {
+      values[index] = value!;
+    });
+  }
+}
+
+class TestRadioButton extends StatefulWidget {
+  const TestRadioButton({super.key});
+
+  @override
+  State<TestRadioButton> createState() => _TestRadioButtonState();
+}
+
+enum TestTestValue {
+  test1,
+  test2,
+  test3;
+}
+
+class _TestRadioButtonState extends State<TestRadioButton> {
+  TestTestValue? selectValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Radio<TestTestValue>(
+            value: TestTestValue.test1,
+            groupValue: selectValue,
+            onChanged: (value) => setState(() => selectValue = value!),
+          ),
+          title: Text(TestTestValue.test1.name),
+          onTap: () => setState(() {
+            if (selectValue != TestTestValue.test1) {
+              selectValue = TestTestValue.test1;
+            }
+          }),
+        ),
+        ListTile(
+          leading: Radio<TestTestValue>(
+            value: TestTestValue.test2,
+            groupValue: selectValue,
+            onChanged: (value) => setState(() => selectValue = value!),
+          ),
+          title: Text(TestTestValue.test2.name),
+          onTap: () => setState(() {
+            if (selectValue != TestTestValue.test2) {
+              selectValue = TestTestValue.test2;
+            }
+          }),
+        ),
+        ListTile(
+          leading: Radio<TestTestValue>(
+            value: TestTestValue.test3,
+            groupValue: selectValue,
+            onChanged: (value) => setState(() => selectValue = value!),
+          ),
+          title: Text(TestTestValue.test3.name),
+          onTap: () => setState(() {
+            if (selectValue != TestTestValue.test3) {
+              selectValue = TestTestValue.test3;
+            }
+          }),
+        ),
+      ],
+    );
+  }
+}
+
+class TestSlider extends StatefulWidget {
+  const TestSlider({super.key});
+
+  @override
+  State<TestSlider> createState() => _TestSliderState();
+}
+
+class _TestSliderState extends State<TestSlider> {
+  double value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('${value.round()}'),
+        Slider(
+          value: value,
+          onChanged: (newValue) => setState(() => value = newValue),
+          divisions: 100,
+          max: 100,
+          min: 0,
+          label: value.round().toString(),
+        ),
+      ],
+    );
+  }
+}
+
+class TestSwitch extends StatefulWidget {
+  const TestSwitch({super.key});
+
+  @override
+  State<TestSwitch> createState() => _TestSwitchState();
+}
+
+class _TestSwitchState extends State<TestSwitch> {
+  bool value = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Switch(value: value, onChanged: (newValue) => setState(() => value = newValue)),
+        CupertinoSwitch(value: value, onChanged: (newValue) => setState(() => value = newValue)),
+      ],
+    );
+  }
+}
+
+class TestPopupMenu extends StatefulWidget {
+  const TestPopupMenu({super.key});
+
+  @override
+  State<TestPopupMenu> createState() => _TestPopupMenuState();
+}
+
+class _TestPopupMenuState extends State<TestPopupMenu> {
+  TestTestValue selectValue = TestTestValue.test1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(selectValue.name),
+        PopupMenuButton(
+          itemBuilder: (context) =>
+              TestTestValue.values.map((value) => PopupMenuItem(value: value, child: Text(value.name))).toList(),
+          onSelected: (newValue) => setState(() => selectValue = newValue),
+        ),
+      ],
+    );
+  }
+}
